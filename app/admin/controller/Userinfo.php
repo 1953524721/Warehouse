1a682fb618af4acc3e38fbd396758e317d4fae4a
liuke <1953524921@qq.com>
2024-12-14 19:08:04 +0800

    添加了一些功能

--- app/admin/controller/Userinfo.php
+++ app/admin/controller/Userinfo.php
@@ -4,28 +4,37 @@ namespace app\admin\controller;
 
 use app\BaseController;
 use app\Request;
+use app\admin\model\UserInfo as UserInfoModel;
 
 class Userinfo extends BaseController
 {
     public function updateUser(Request $request): \think\response\Json
     {
-        $data['id']              = $request->param('id');
-        $data['name']            = $request->param('name');
-        $data['corporate_name']  = $request->param('corporate_name');
-        $data['phone']           = $request->param('phone');
-        $data['base']            = $request->param('base');
-        $data['status']          = $request->param('status');
-        $data['belongs']         = $request->param('belongs');
-        $data['email']           = $request->param('email');
+        $id              = $request->param('id');
+        $name            = $request->param('name');
+        $corporate_name  = $request->param('corporate_name');
+        $phone           = $request->param('phone');
+        $base            = $request->param('base');
+        $status          = $request->param('status');
+        $belongs         = $request->param('belongs');
+        $email           = $request->param('email');
 
-        return json($data);
-
-
-
-
-
-
-
-
+        $userInfoModel = new UserInfoModel();
+        $result = $userInfoModel->updateUserInfo($id,$name,$corporate_name,$phone,$base,$status,$belongs,$email);
+        if ($result)
+        {
+            return json(['status' => 'success', 'message' => '更新成功']);
+        }
+        else
+        {
+            return json(['status' => 'error', 'message' => '更新失败']);
+        }
     }
+
+
+
+
+
+
+
 }
